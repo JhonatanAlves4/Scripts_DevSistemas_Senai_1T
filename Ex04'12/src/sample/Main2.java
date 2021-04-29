74d485b0d4568d7149c538d76d55a7cce40a2b11
JhonatanAlves4 <71884829+JhonatanAlves4@users.noreply.github.com>
2021-04-29 17:05:04 -0300

    Update Main2.java

--- Ex04'12/src/sample/Main2.java
+++ Ex04'12/src/sample/Main2.java
@@ -9,17 +9,51 @@ import java.util.Scanner;
 public class Main2 {
 
     public static void main(String[] args) {
-
         Scanner leitor = new Scanner(System.in);
-        Produtos produtos = new Produtos("Rice",1000);
-        System.out.println(produtos.imprimir());
 
-        Importados imp = new Importados("Chains",8000);
-        imp.setCustomsFee(20);
-        System.out.println("\n" + imp.imprimir() + imp.imprimir2());
+        System.out.println("Entre com o número de produtos: ");
+        int numberP = leitor.nextInt();
 
-        Usados usados = new Usados("Gloves", 4);
-        usados.setManufactureDate("21/04/2021");
-        System.out.println("\n" + usados.imprimir() + usados.littleDate());
+        for (int i = 0; i < numberP; i++) {
+            System.out.println("Comum, usado ou importado? (c, u, i)");
+            char cui = leitor.next().charAt(0);
+
+            if (cui == 'c') {
+                Produtos produtos = new Produtos();
+                System.out.println("Qual o nome do produto?");
+                String nome = leitor.next();
+                produtos.setName(nome);
+                System.out.println("Qual o preço do produto?");
+                int preco = leitor.nextInt();
+                produtos.setPrice(preco);
+
+                System.out.println(produtos.imprimir());
+
+            } else if (cui == 'i') {
+                Importados imp = new Importados();
+                System.out.println("Qual o nome do produto importado?");
+                String nome = leitor.next();
+                imp.setName(nome);
+                System.out.println("Qual o preço do produto importado?");
+                int preco = leitor.nextInt();
+                imp.setPrice(preco);
+                imp.setCustomsFee(20);
+
+                System.out.println("\n" + imp.imprimir() + imp.imprimir2());
+
+            } else if (cui == 'u') {
+                Usados usados = new Usados();
+                System.out.println("Qual o nome do produto usado?");
+                String nome = leitor.next();
+                usados.setName(nome);
+                System.out.println("Qual o preço do produto usado?");
+                int preco = leitor.nextInt();
+                usados.setPrice(preco);
+                System.out.println("Por favor, insira a data de fabricação");
+                String data = leitor.next();
+                usados.setManufactureDate(data);
+                System.out.println("\n" + usados.imprimir() + usados.littleDate());
+            }
+        }
     }
 }
